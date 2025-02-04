20b72f340af60879c397439355db28a2b105a181
eloe <masterthemanok@gmail.com>
2025-02-04 23:08:46 +0800

    fixed conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import Image from "next/image";
-import {useState, useEffect} from "react";
-import {BarChartHorizontal} from "@/components/chart-bar-horizontal";
-import {DropDownMenu} from "@/components/ui/radio-group";
-import {PieChartWithLabels} from "@/components/piechart-withlabels";
-import {isArray} from "google-spreadsheet/src/lib/lodash";
+import { useState, useEffect, createContext } from "react";
+import { BarChartHorizontal } from "@/components/chart-bar-horizontal";
+import { DropDownMenu } from "@/components/ui/radio-group";
+import { PieChartWithLabels } from "@/components/piechart-withlabels";
+import { isArray } from "google-spreadsheet/src/lib/lodash";
 
 function fetchAll() {
   return fetch("http://localhost:3000/api/all", {
@@ -76,15 +76,17 @@ function getDataCountForSenate(data) {
   return Object.entries(courseCounts);
 }
 
-export const ChosenSenate = createContext(null);
+export const ChosenSenate = createContext<{
+  senateSelected: string | null;
+  setSenateSelected: React.Dispatch<React.SetStateAction<string | null>>;
+} | null>(null);
 
 export default function Home() {
-  const [senateSelected, setSenateSelected] = useState(null);  
+  const [senateSelected, setSenateSelected] = useState(null);
   const [allData, setAllData] = useState<any[]>([]);
-    const [courseData, setCourseData] = useState<any>(null);
-    const [senatorData, setSenatorData] = useState<any[]>([]);
-    const [totalVotes, setTotalVotes] = useState(0);
-
+  const [courseData, setCourseData] = useState<any>(null);
+  const [senatorData, setSenatorData] = useState<any[]>([]);
+  const [totalVotes, setTotalVotes] = useState(0);
 
   // Fetch data and set up polling
   useEffect(() => {
@@ -101,8 +103,6 @@ export default function Home() {
         });
         setSenatorData(senatorResponse);
         setCourseData(courseResponse);
-        setTotalVotes(allResponse.reduce((sum :number, item : isArray) => sum + item[1], 0));
-
 
         // console.log("All Data:", allResponse);
         console.log("Course Data:", courseResponse);
@@ -120,179 +120,219 @@ export default function Home() {
 
   useEffect(() => {
     console.log("Updated Data being used: ", allData);
+    setTotalVotes(
+      allData.reduce((sum: number, item: isArray) => sum + item[1], 0)
+    );
     console.log("The senator Selected: ", senatorData);
     // console.log("THe courses: ", courseData);
     console.log("The senate selected: ", senateSelected);
   }, [allData, senatorData, senateSelected, courseData]);
 
-    const filterDescriptions = {
-        "All": "Displays all available data without any filtering.",
-        "All Departments": "Shows data related to all departments collectively.",
-        "By Department": "Filters data based on specific departments.",
-        "By Senator":
-            "Filters data based on specific senators or their associated categories.",
-    };
-    const [filterSelected, setFilterSelected] = useState("All");
-    // const choiceSelected= useState("All");
-    return (
-        <div
-            className="bg-cover bg-center bg-[url('/graphics/background.png')] scroll-auto h-auto w-screen flex flex-col justify-center items-center gap-10 p-10">
-            <Image
-                src={"/graphics/title.png"}
-                alt={"Halalan 2025"}
-                width={600}
-                height={600}
-                className="mb-10  cursor-pointer hover:animate-pulse"
-            />
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-                {/*{senatorNames.map((senator, index) => (*/}
-                {/*  <Image*/}
-                {/*    key={index}*/}
-                {/*    src={`/senators/${index + 1}.png`}*/}
-                {/*    alt={`Senator ${senator}`}*/}
-                {/*    width={125}*/}
-                {/*    height={125}*/}
-                {/*    className="self-center w-full sm:w-32 md:w-40 lg:w-44 hover:scale-105"*/}
-                {/*  />*/}
-                {/*))}*/}
+  const filterDescriptions = {
+    All: "Displays all available data without any filtering.",
+    "All Departments": "Shows data related to all departments collectively.",
+    "By Department": "Filters data based on specific departments.",
+    "By Senator":
+      "Filters data based on specific senators or their associated categories.",
+  };
+  const [filterSelected, setFilterSelected] = useState("All");
+  // const choiceSelected= useState("All");
+  return (
+    <div className="bg-cover bg-center bg-[url('/graphics/background.png')] scroll-auto h-auto w-screen flex flex-col justify-center items-center gap-10 p-10">
+      <Image
+        src={"/graphics/title.png"}
+        alt={"Halalan 2025"}
+        width={600}
+        height={600}
+        className="mb-10  cursor-pointer hover:animate-pulse"
+      />
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {/*{senatorNames.map((senator, index) => (*/}
+        {/*  <Image*/}
+        {/*    key={index}*/}
+        {/*    src={`/senators/${index + 1}.png`}*/}
+        {/*    alt={`Senator ${senator}`}*/}
+        {/*    width={125}*/}
+        {/*    height={125}*/}
+        {/*    className="self-center w-full sm:w-32 md:w-40 lg:w-44 hover:scale-105"*/}
+        {/*  />*/}
+        {/*))}*/}
 
-                {
-                    allData
-                        .sort((a, b) => b[1] - a[1])
-                        .slice(0, 12)
-                        .map((senator, index) => (
-                            <div key={index}
-                                 className='flex items-center bg-[#FDFDFD]  drop-shadow-lg rounded-lg gap-7 justify-between p-3 hover:scale-105'>
-                                <div className='flex flex-row items-center gap-5'>
-                                    <p className={`font-bold ${index + 1 < 10 ? 'text-5xl' : 'text-3xl'}`}>{index + 1}</p>
-                                    <Image src={`/senators/${senator[0].split('.')[0]}.png`}
-                                           alt={senator[0].split('.')[1]} height={50} width={50}
-                                           className='rounded-full bg-gray-500 shadow-md'/>
-                                    <p className={`font-bold ${senator[0].split('.')[1].length > 30 ? 'text-md' : 'text-xl'}`}>{senator[0].split('.')[1]}</p>
-                                </div>
-                                <p className='justify-end font-bold text-2xl'>
-                                    {totalVotes ? ((senator[1] / totalVotes) * 100).toFixed(1) + "%" : "0%"}
-                                </p>
-
-                            </div>
-                        ))
-                }
-            </div>
+        {allData
+          .sort((a, b) => b[1] - a[1])
+          .slice(0, 12)
+          .map((senator, index) => (
             <div
-                id=""
-                className="w-full flex flex-col lg:flex-row justify-center  pl-5 pr-5 items-start gap-5"
+              key={index}
+              className="flex items-center bg-[#FDFDFD]  drop-shadow-lg rounded-lg gap-7 justify-between p-3 hover:scale-105"
             >
-                <div
-                    id="filters"
-                    className="bg-[#1A1A1A] h-auto  xl:w-1/3 2xl:w-1/3 lg:w-1/3 w-full  rounded-md "
+              <div className="flex flex-row items-center gap-5">
+                <p
+                  className={`font-bold ${
+                    index + 1 < 10 ? "text-5xl" : "text-3xl"
+                  }`}
                 >
-                    <header className="p-3 gap-2 flex flex-row">
-                        <Image
-                            src={"/graphics/filter.png"}
-                            width={25}
-                            height={10}
-                            alt={"Filter icon"}
-                        />
-                        <p className="text-white font-bold font-sans lg:text-3xl sm-text-2xl cursor-pointer">
-                            FILTERS
-                        </p>
-                    </header>
-                    <hr/>
-                    <div key={"All"} onClick={() => setFilterSelected("All")}>
-                        <div
-                            className={`text-white p-3 cursor-pointer hover:bg-[#141414]  ${
-                                filterSelected === "All" ? "bg-[#333333]" : ""
-                            }`}
-                        >
-                            <p>All</p>
-                        </div>
-                        <hr/>
-                    </div>
-                    <div key={"All Departments"} onClick={() => setFilterSelected("All Departments")}>
-                        <div
-                            className={`text-white p-3 cursor-pointer hover:bg-[#141414]  ${
-                                filterSelected === "All Departments" ? "bg-[#333333]" : ""
-                            }`}
-                        >
-                            <p>All Departments</p>
-                        </div>
-                        <hr/>
-                    </div>
-                    <div key={"By Department"} onClick={() => setFilterSelected("By Department")}>
-                        <div
-                            className={`text-white p-3 cursor-pointer hover:bg-[#141414]  ${
-                                filterSelected === "By Department" ? "bg-[#333333]" : ""
-                            }`}
-                        >
-                            <p>By Department</p>
-                        </div>
-                    </div>
-                    <div
-                        className={`
-    overflow-hidden transition-all duration-500 ease-in-out flex items-center justify-center
-    ${filterSelected === "By Department" ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0 pointer-events-none"}
-  `}
-                    >
-                        {/* Inner container with padding */}
-                        <div className="p-10">
-                        
-            >
-                            <DropDownMenu senatorNames={allData}/>
-                          
-                        </div>
-                    </div>
-
-
-                    <div key={"By Senator"} onClick={() => setFilterSelected("By Senator")}>
-                        <div
-                            className={`text-white p-3 cursor-pointer hover:bg-[#141414]  ${
-                                filterSelected === "By Senator" ? "bg-[#333333]" : ""
-                            }`}
-                        >
-                            <p>By Senator</p>
-                        </div>
-                        <hr/>
-                    </div>
-
-                    <div
-                        className={`
-    overflow-hidden transition-all duration-500 flex items-center  justify-center ease-in-out
-    ${filterSelected === "By Senator" ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0 pointer-events-none"}
-  `}
-                    >
-
-                          
-                        {/* Inner container with padding */}
-                        <div className="p-10">
-                        <ChosenSenate.Provider
-              value={{ senateSelected, setSenateSelected }}
-            >
-                            <DropDownMenu senatorNames={allData}/>
-                            </ChosenSenate.Provider>
-                        </div>
-                    </div>
-                </div>
-                <div
-                    id="charts"
-                    className="bg-[#1A1A1A] w-full  h-full rounded-md p-5 flex flex-col gap-5 bg-opacity-50"
+                  {index + 1}
+                </p>
+                <Image
+                  src={`/senators/${senator[0].split(".")[0]}.png`}
+                  alt={senator[0].split(".")[1]}
+                  height={50}
+                  width={50}
+                  className="rounded-full bg-gray-500 shadow-md"
+                />
+                <p
+                  className={`font-bold ${
+                    senator[0].split(".")[1].length > 30 ? "text-md" : "text-xl"
+                  }`}
                 >
-                    {filterSelected === "All Departments" ? (
-                        <PieChartWithLabels/>
-                    ) : filterSelected === "By Senator" ? (
-                        <BarChartHorizontal
-                            title={filterSelected}
-                            description={filterDescriptions[filterSelected]}
-                            data={getDataCountForSenate(senatorData)}
-                        />
-                    ) : (
-                        <BarChartHorizontal
-                            title={filterSelected}
-                            description={filterDescriptions[filterSelected]}
-                            data={allData}
-                        />
-                    )}
-                </div>
+                  {senator[0].split(".")[1]}
+                </p>
+              </div>
+              <p className="justify-end font-bold text-2xl">
+                {totalVotes
+                  ? ((senator[1] / totalVotes) * 100).toFixed(1) + "%"
+                  : "0%"}
+              </p>
             </div>
+          ))}
+      </div>
+      <div
+        id=""
+        className="w-full flex flex-col lg:flex-row justify-center  pl-5 pr-5 items-start gap-5"
+      >
+        <div
+          id="filters"
+          className="bg-[#1A1A1A] h-auto  xl:w-1/3 2xl:w-1/3 lg:w-1/3 w-full  rounded-md "
+        >
+          <header className="p-3 gap-2 flex flex-row">
+            <Image
+              src={"/graphics/filter.png"}
+              width={25}
+              height={10}
+              alt={"Filter icon"}
+            />
+            <p className="text-white font-bold font-sans lg:text-3xl sm-text-2xl cursor-pointer">
+              FILTERS
+            </p>
+          </header>
+          <hr />
+          <div key={"All"} onClick={() => setFilterSelected("All")}>
+            <div
+              className={`text-white p-3 cursor-pointer hover:bg-[#141414]  ${
+                filterSelected === "All" ? "bg-[#333333]" : ""
+              }`}
+            >
+              <p>All</p>
+            </div>
+            <hr />
+          </div>
+          <div
+            key={"All Departments"}
+            onClick={() => setFilterSelected("All Departments")}
+          >
+            <div
+              className={`text-white p-3 cursor-pointer hover:bg-[#141414]  ${
+                filterSelected === "All Departments" ? "bg-[#333333]" : ""
+              }`}
+            >
+              <p>All Departments</p>
+            </div>
+            <hr />
+          </div>
+          <div
+            key={"By Department"}
+            onClick={() => setFilterSelected("By Department")}
+          >
+            <div
+              className={`text-white p-3 cursor-pointer hover:bg-[#141414]  ${
+                filterSelected === "By Department" ? "bg-[#333333]" : ""
+              }`}
+            >
+              <p>By Department</p>
+            </div>
+          </div>
+          <div
+            className={`
+    overflow-hidden transition-all duration-500 ease-in-out flex items-center justify-center
+    ${
+      filterSelected === "By Department"
+        ? "max-h-[500px] opacity-100"
+        : "max-h-0 opacity-0 pointer-events-none"
+    }
+  `}
+          >
+            {/* Inner container with padding */}
+            <div className="p-10">
+              <ChosenSenate.Provider
+                value={{ senateSelected, setSenateSelected }}
+              >
+                <DropDownMenu senatorNames={allData} />
+              </ChosenSenate.Provider>
+            </div>
+          </div>
+
+          <div
+            key={"By Senator"}
+            onClick={() => setFilterSelected("By Senator")}
+          >
+            <div
+              className={`text-white p-3 cursor-pointer hover:bg-[#141414]  ${
+                filterSelected === "By Senator" ? "bg-[#333333]" : ""
+              }`}
+            >
+              <p>By Senator</p>
+            </div>
+            <hr />
+          </div>
+
+          <div
+            className={`
+    overflow-hidden transition-all duration-500 flex items-center  justify-center ease-in-out
+    ${
+      filterSelected === "By Senator"
+        ? "max-h-[500px] opacity-100"
+        : "max-h-0 opacity-0 pointer-events-none"
+    }
+  `}
+          >
+            {/* Inner container with padding */}
+            <div className="p-10">
+              <ChosenSenate.Provider
+                value={{ senateSelected, setSenateSelected }}
+              >
+                <DropDownMenu senatorNames={allData} />
+              </ChosenSenate.Provider>
+            </div>
+          </div>
         </div>
-    );
+        <div
+          id="charts"
+          className="bg-[#1A1A1A] w-full  h-full rounded-md p-5 flex flex-col gap-5 bg-opacity-50"
+        >
+          {filterSelected === "All Departments" ? (
+            <PieChartWithLabels />
+          ) : filterSelected === "By Senator" ? (
+            !senatorData ? (
+              <p className="text-white font-bold font-sans lg:text-3xl sm-text-2xl cursor-pointer">
+                Choose a senator
+              </p>
+            ) : (
+              <BarChartHorizontal
+                title={filterSelected}
+                description={filterDescriptions[filterSelected]}
+                data={getDataForSenate(senatorData)}
+              />
+            )
+          ) : (
+            <BarChartHorizontal
+              title={filterSelected}
+              description={filterDescriptions[filterSelected]}
+              data={allData}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
