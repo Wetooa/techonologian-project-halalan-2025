a33b1b8782e504b493a8bf72415a6a393a1c1306
drkcutie <derrick.binangbang@cit.edu>
2025-02-04 17:08:10 +0800

    top 12 done

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import {useState, useEffect} from "react";
 import {BarChartHorizontal} from "@/components/chart-bar-horizontal";
 import {DropDownMenu} from "@/components/ui/radio-group";
 import {PieChartWithLabels} from "@/components/piechart-withlabels";
+import {isArray} from "google-spreadsheet/src/lib/lodash";
 
 function fetchAll() {
     return fetch("http://localhost:3000/api/all", {
@@ -61,11 +62,11 @@ function fetchBySenator(senatorNumber: string) {
             console.error("There was a problem fetching results bySenator", error);
         });
 }
-
 export default function Home() {
     const [allData, setAllData] = useState<any[]>([]);
     const [courseData, setCourseData] = useState<any>(null);
     const [senatorData, setSenatorData] = useState<any[]>([]);
+    const [totalVotes, setTotalVotes] = useState(0);
 
     // Fetch data and set up polling
     useEffect(() => {
@@ -74,12 +75,12 @@ export default function Home() {
                 const allResponse = await fetchAll();
                 const courseResponse = await fetchByCourse("BSCS");
                 const senatorResponse = await fetchBySenator("2");
-
                 setAllData(() => {
                     return allResponse;
                 });
                 setSenatorData(senatorResponse);
                 setCourseData(courseResponse);
+                setTotalVotes(allResponse.reduce((sum :number, item : isArray) => sum + item[1], 0));
 
                 // console.log("All Data:", allResponse);
                 console.log("Course Data:", courseResponse);
@@ -120,7 +121,7 @@ export default function Home() {
                 height={600}
                 className="mb-10  cursor-pointer hover:animate-pulse"
             />
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4">
                 {/*{senatorNames.map((senator, index) => (*/}
                 {/*  <Image*/}
                 {/*    key={index}*/}
@@ -131,12 +132,38 @@ export default function Home() {
                 {/*    className="self-center w-full sm:w-32 md:w-40 lg:w-44 hover:scale-105"*/}
                 {/*  />*/}
                 {/*))}*/}
+
+                {
+                    allData
+                        .sort((a, b) => b[1] - a[1])
+                        .slice(0, 12)
+                        .map((senator, index) => (
+                            <div key={index}
+                                 className='flex items-center bg-[#FDFDFD]  drop-shadow-lg rounded-lg gap-7 justify-between p-3 hover:scale-105'>
+                                <div className='flex flex-row items-center gap-5'>
+                                    <p className={`font-bold ${index + 1 < 10 ? 'text-5xl' : 'text-3xl'}`}>{index + 1}</p>
+                                    <Image src={`/senators/${senator[0].split('.')[0]}.png`}
+                                           alt={senator[0].split('.')[1]} height={50} width={50}
+                                           className='rounded-full bg-gray-500 shadow-md'/>
+                                    <p className={`font-bold ${senator[0].split('.')[1].length > 30 ? 'text-md' : 'text-xl'}`}>{senator[0].split('.')[1]}</p>
+                                </div>
+                                <p className='justify-end font-bold text-2xl'>
+                                    {totalVotes ? ((senator[1] / totalVotes) * 100).toFixed(1) + "%" : "0%"}
+                                </p>
+
+                            </div>
+                        ))
+                }
+
+
             </div>
             <div
                 id=""
                 className="w-full flex flex-col lg:flex-row justify-center  pl-5 pr-5 items-start gap-5"
             >
-                <div
+
+
+            <div
                     id="filters"
                     className="bg-[#1A1A1A] h-auto  xl:w-1/3 2xl:w-1/3 lg:w-1/3 w-full  rounded-md "
                 >
