3cf98aa5e1bee44fba03ed8c12fa7dd29a2e1ff1
drkcutie <derrick.binangbang@cit.edu>
2025-02-04 08:57:30 +0800

    added function for fetching datas

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import {useState} from "react";
 import {BarChartHorizontal} from "@/components/chart-bar-horizontal";
 import {DropDownMenu} from "@/components/ui/radio-group";
 import { PieChartWithLabels } from "@/components/piechart-withlabels";
+import SenatorCard from "@/components/ui/senator-card";
 
 
 function fetchAll(){
@@ -65,12 +66,7 @@ function fetchBySenator(senatorNumber : string){
         });
 }
 export default function Home() {
-
     fetchAll();
-    fetchBySenator("1");
-    fetchByCourse("BSCS");
-
-
     const senatorNames = [
         "Bam Aquino",
         "Pangalinan Kiko",
@@ -83,7 +79,7 @@ export default function Home() {
         "Lacson Ping",
         "Casiño, Teddy & Marquez, Norman"
     ];
-    const filters = ["All", "All Departments", "All Voter's Detail", "By Department", "By Senator"];
+    const filters = ["All", "All Departments",  "By Department", "By Senator"];
     const filterDescriptions = {
         "All": "Displays all available data without any filtering.",
         "All Departments": "Shows data related to all departments collectively.",
@@ -91,27 +87,15 @@ export default function Home() {
         "By Department": "Filters data based on specific departments.",
         "By Senator": "Filters data based on specific senators or their associated categories."
     };
-    const [filterSelected, setFilterSelected] = useState(0);
-    // const choiceSelected= useState("All");
-    const getFilter = () => {
-        return filters[filterSelected];
-    }
+    const [filterSelected, setFilterSelected] = useState("All");
     return (
         <div
             className="bg-cover bg-center bg-[url('/graphics/background.png')] scroll-auto h-auto w-screen flex flex-col justify-center items-center gap-10 p-10">
             <Image src={'/graphics/title.png'} alt={"Halalan 2025"} width={600} height={600}
                    className="mb-10  cursor-pointer hover:animate-pulse"/>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-                {senatorNames.map((senator, index) => (
-                    <Image
-                        key={index}
-                        src={`/senators/${index + 1}.png`}
-                        alt={`Senator ${senator}`}
-                        width={125}
-                        height={125}
-                        className="self-center w-full sm:w-32 md:w-40 lg:w-44 hover:scale-105"
-                    />
-                ))}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                <SenatorCard senatorName={"1"} position={1} votePercentage={"99"}/>
+                <SenatorCard senatorName={"2"} position={2} votePercentage={"95"}/>
             </div>
             <div id="" className="w-full flex flex-col lg:flex-row justify-center  pl-5 pr-5 items-start gap-5">
                 <div id='filters' className="bg-[#1A1A1A] h-auto  xl:w-1/3 2xl:w-1/3 lg:w-1/3 w-full  rounded-md ">
@@ -123,26 +107,26 @@ export default function Home() {
                     </header>
                     <hr/>
                     {filters.map((filter, index) => (
-                        <div key={index} onClick={() => setFilterSelected(index)}>
+                        <div key={index} onClick={() => setFilterSelected(filter)}>
                             <div
-                                className={`text-white p-3 cursor-pointer hover:bg-[#141414]  ${filterSelected === index ? 'bg-[#333333]' : ''}`}>
+                                className={`text-white p-3 cursor-pointer hover:bg-[#141414]  ${filterSelected === filter ? 'bg-[#333333]' : ''}`}>
                                 <p>{filter}</p>
                             </div>
                             <hr/>
                         </div>
                     ))}
-                    <div className= {`flex justify-center items-center p-10 ${filters[filterSelected] === "By Senator" ? '' : 'hidden' }`}>
+                    <div className= {`flex justify-center items-center p-10 ${filterSelected === "By Senator" ? '' : 'hidden' }`}>
                         <DropDownMenu senatorNames = {senatorNames} />
                     </div>
 
                 </div>
                 <div id='charts' className="bg-[#1A1A1A] w-full  h-full rounded-md p-5 flex flex-col gap-5 bg-opacity-50">
                     {
-                        getFilter() === "All Departments"
+                        filterSelected === "All Departments"
                             ? <PieChartWithLabels />
                             : <BarChartHorizontal
-                                title={filters[filterSelected]}
-                                description={filterDescriptions[filters[filterSelected]]}
+                                title={filterSelected}
+                                description={filterDescriptions[filterSelected]}
                             />
                     }
 
